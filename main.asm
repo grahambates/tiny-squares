9d8e566bf67f873544db9f8cab4ebf2aeb8f6e09
Graham Bates <info@grahambates.com>
2024-01-05 09:23:40 +0000

    wip

--- main.asm
+++ main.asm
@@ -5,18 +5,17 @@
 Main:
                 move.l  $170(a2),a6     ; intuitionbase from globvec
                 move.l  ib_ActiveScreen(a6),a5
-
-.gfx:           move.l  LN_PRED(a6),a6  ; graphics is before intuition, or we crash...
-                move.l  LN_NAME(a6),a0
-                cmp.w   #"gr",(a0)
-                bne.b   .gfx
+                move.l  $4.w,a6         ; execbase
+                move.l  156(a6),a6      ; graphics.library
 
                 lea     sc_RastPort(a5),a1
                 jsr     _LVOClearScreen(a6)
 .l:
+                jsr     _LVOWaitTOF(a6)
                 addq.w  #1,d4           ; inc frame
                 move.w  d4,d0
                 lsr.w   #7,d0
+                lea     sc_RastPort(a5),a1 ; WaitTOF trashed this :-(
                 jsr     _LVOSetAPen(a6)
                 clr.w   d7
 .y:
@@ -24,7 +23,7 @@ Main:
                 eor.w   d7,d0
                 add.w   d0,d0           ; correct aspect
                 move.w  d7,d1           ; y
-                add.w   #64,d0          ; center
+                ; add.w   #64,d0          ; center
                 jsr     _LVOWritePixel(a6)
 
                 addq.b  #1,d7
